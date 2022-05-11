355d30e38ba4911b682018528560b9a6d2db1cf8
zhangruitao <zhangruitao@megvii.com>
2022-05-11 13:39:02 +0800

    [update] Unit 5 end

--- rust_stu/src/main.rs
+++ rust_stu/src/main.rs
@@ -4,6 +4,23 @@ use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // 常量，必须声明类型
+
+    let x: (i32, f64, u8) = (500, 6.4, 1); // tuple
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5]; // 数组
+    let a = [3; 5]; // let a = [3, 3, 3, 3, 3];
+    let first = a[0];
+    let second = a[1];
+
+    println!("add func: {}", add(1, 3));
+
+    control();
+
+    string();
 
     let secret_number = rand::thread_rng().gen_range(1..101); // (1..=100)
 
@@ -34,4 +51,254 @@ fn main() {
             }
         }
     }
+}
+
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+
+fn control() {
+    // 1
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // 2
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+
+    // 3
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up; // break 外面的 loop
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    // 4
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // 5
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // 6
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+
+    // 7
+    for number in (1..4).rev() { // rev 用于翻转
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
+
+
+fn string() {
+    // 1
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() 在字符串后追加字面值
+
+    println!("{}", s); // 将打印 `hello, world!`
+
+    // s2 会覆盖 s1，再次调用 s1 会报错
+    /*
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    println!("{}, world!", s1);
+    */
+
+    // 2
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+}
+
+
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len(); // len() 返回字符串的长度
+
+//     (s, length)
+// }
+
+// 使用引用可以只用变量的值，而没有所有权，因此在使用后不会释放掉该变量
+fn calculate_length(s: &String) -> usize { 
+    s.len()
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn slice() {
+    let my_string = String::from("hello world");
+
+    // `first_word` 适用于 `String`（的 slice），整体或全部
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` 也适用于 `String` 的引用，
+    // 这等价于整个 `String` 的 slice
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` 适用于字符串字面值，整体或全部
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // 因为字符串字面值已经 **是** 字符串 slice 了，
+    // 这也是适用的，无需 slice 语法！
+    let word = first_word(my_string_literal);
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn struct_test() {
+    // 1
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
+
+    // 2 简写
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+} 
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+// debug
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+//     dbg!(&rect1);
+// }
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // 方法
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    // 关联函数
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn method_test() {
+    // 方法
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    // 关联函数
+    let sq = Rectangle::square(3);
 }
